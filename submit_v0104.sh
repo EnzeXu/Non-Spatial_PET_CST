885a85ae9ad7bb5e5850e6e97f9520da689c43c1
EnzeXu <xue20@wfu.edu>
2023-01-04 00:40:33 -0600

    updates

---
+++ submit_v0104.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+sbatch jobs/GA_A1_1000.slurm
+sbatch jobs/GA_A1_2000.slurm
+sbatch jobs/GA_A1_3000.slurm
+sbatch jobs/GA_A1_4000.slurm
+sbatch jobs/GA_A1_5000.slurm
+sbatch jobs/GA_A1_6000.slurm
+sbatch jobs/GA_A2_1000.slurm
+sbatch jobs/GA_A2_2000.slurm
+sbatch jobs/GA_A2_3000.slurm
+sbatch jobs/GA_A2_4000.slurm
+sbatch jobs/GA_A2_5000.slurm
+sbatch jobs/GA_A2_6000.slurm
