5c5dda1a2955d417ff5d202305ec1ebb9a866d99
EnzeXu <xue20@wfu.edu>
2023-01-10 22:24:19 -0500

    update bash script

---
+++ submit_v0110-3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sbatch jobs/GA_A4-1_500.slurm
+sbatch jobs/GA_A4-1_600.slurm
+sbatch jobs/GA_A4-1_700.slurm
+sbatch jobs/GA_A4-1_800.slurm
+sbatch jobs/GA_A4-1_900.slurm
+sbatch jobs/GA_A4-1_1000.slurm
+
+#sbatch jobs/GA_A3-1_500.slurm
+#sbatch jobs/GA_A3-1_1000.slurm
+#sbatch jobs/GA_A3-1_1500.slurm
+#sbatch jobs/GA_A3-1_2000.slurm
+
+#sbatch jobs/GA_A3-2_500.slurm
+#sbatch jobs/GA_A3-2_1000.slurm
+#sbatch jobs/GA_A3-2_1500.slurm
+#sbatch jobs/GA_A3-2_2000.slurm
+
+#sbatch jobs/GA_A4-1_500.slurm
+#sbatch jobs/GA_A4-1_1000.slurm
+#sbatch jobs/GA_A4-1_1500.slurm
+#sbatch jobs/GA_A4-1_2000.slurm
+
+#sbatch jobs/GA_A4-2_500.slurm
+#sbatch jobs/GA_A4-2_1000.slurm
+#sbatch jobs/GA_A4-2_1500.slurm
+#sbatch jobs/GA_A4-2_2000.slurm
