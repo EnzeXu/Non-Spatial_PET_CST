ed919da5bd545c6e68a5a35517e0e94e0edfeda4
EnzeXu <xue20@wfu.edu>
2023-01-19 20:35:24 -0500

    resubmit and clear repo

--- test_brkga.py
+++
@@ -1,4 +0,0 @@
-from simulation import simulate
-
-if __name__ == "__main__":
-    simulate(pop_size=30, generation=100, method="BRKGA")
--- test_de.py
+++
@@ -1,4 +0,0 @@
-from simulation import simulate
-# from pymoo.operators.sampling.lhs import LHS
-if __name__ == "__main__":
-    simulate(pop_size=30, generation=100, method="DE")
